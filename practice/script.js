5a7e207e3409238bcc99083f35d7f0fc36e118bf
kjason-dev <kjason.dev@gmail.com>
2023-02-18 23:55:50 +0900

    api 연동 - 데이터 입력하기

--- practice/script.js
+++ practice/script.js
@@ -5,7 +5,10 @@
     return document.querySelector(target)
   }
 
+  const API_URL = 'http://localhost:3000/todos'
   const $todos = get('.todos')
+  const $form = get('.todo_form')
+  const $todoInput = get('.todo_input')
 
   const createTodoElement = (item) => {
     const { id, content } = item
@@ -50,16 +53,38 @@
   }
 
   const getTodos = () => {
-    fetch('http://localhost:3000/todos')
+    fetch(API_URL)
       .then((response) => response.json())
       .then((todos) => renderAllTodos(todos))
       .catch((error) => console.error(error))
+  }
+
+  const addTodo = (e) => {
+    e.preventDefault()
+    const todo = {
+      content: $todoInput.value,
+      completed: false,
+    }
+    fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(todo),
+    })
+    .then(getTodos)
+    .then(() => {
+      $todoInput.value = ''
+      $todoInput.value.focus()
+    })
+    .catch((error) => console.error(error))
   }
 
   const init = () => {
     window.addEventListener('DOMContentLoaded', () => {
       getTodos()
     })
+    $form.addEventListener('submit', addTodo)
   }
   init()
 })()
